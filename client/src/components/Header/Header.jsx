7ecfca7746b6ef59e67f5426195d7d74a29169d7
Abhishauk <ukabhisha@gmail.com>
2024-09-27 23:17:42 +0530

    header setup

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,17 +5,20 @@ import { getMenuStyles } from "../../utils/common";
 import useHeaderColor from "../../hooks/useHeaderColor";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import ProfileMenu from "../ProfileMenu/ProfileMenu";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const headerColor = useHeaderColor();
+  const { loginWithRedirect , isAuthenticated , user ,logout} = useAuth0();
 
   return (
     <section className="h-wrapper" style={{ background: headerColor }}>
       <div className="flexCenter innerWidth paddings h-container">
         {/* logo */}
         <Link to="/">
-        <img src="./logo.png" alt="logo" width={100} />
+          <img src="./logo.png" alt="logo" width={100} />
         </Link>
         {/* menu */}
         <OutsideClickHandler
@@ -28,20 +31,24 @@ const Header = () => {
             className="flexCenter h-menu"
             style={getMenuStyles(menuOpened)}
           >
-           <NavLink to = "/properties">Properties</NavLink>
-              <a href="mailto:zainkeepscode@gmail.com">Contact</a>
+            <NavLink to="/properties">Properties</NavLink>
+            <a href="mailto:zainkeepscode@gmail.com">Contact</a>
             {/* {login button} */}
-            <button className="button">
+           { 
+           !isAuthenticated ?
+
+            <button className="button" onClick={loginWithRedirect}>
               Login
-            </button>
+            </button> :
+            <div>
+             {/* <ProfileMenu  user = { user } logout = { logout }/> */}
+            </div>
+            }
           </div>
         </OutsideClickHandler>
 
         {/* for medium and small screens */}
-        <div
-          className="menu-icon"
-          onClick={() => setMenuOpened((prev) => !prev)}
-        >
+        <div className="menu-icon" onClick={() => setMenuOpened(prev => !prev)}>
           <BiMenuAltRight size={30} />
         </div>
       </div>
